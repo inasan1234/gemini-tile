d880fd7bee2abba30723f7591e9f996198547741
inasan1234 <138194052+inasan1234@users.noreply.github.com>
2025-03-12 23:05:03 +0900

    first-commit

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import { useEffect } from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/health_check')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error('Error:', error))
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
